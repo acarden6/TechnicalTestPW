71e3cd1680715effdf7f6b73434eb50c4a35e312
Alejandra <acarden6@eafit.edu.co>
2021-04-02 03:32:15 -0500

    Casi finalizado

--- GentePreValente/resources/views/layouts/master.blade.php
+++ GentePreValente/resources/views/layouts/master.blade.php
@@ -1,45 +1,35 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="es">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="generator" content="Jekyll v4.1.1">
     <title>Gente PreValente</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/album/">
-
-
     <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-    <link href="{{ asset('/css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/css/styleMaster.css') }}" rel="stylesheet" />
 
-    <!-- Favicons -->
-    <link rel="apple-touch-icon" href="/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-    <link rel="manifest" href="/docs/4.5/assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
-    <link rel="icon" href="/docs/4.5/assets/img/favicons/favicon.ico">
-    <meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#563d7c">
+
 
     <!-- Custom styles for this template -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
 
-    <link href="album.css" rel="stylesheet">
+</head>
 
-  </head>
-  <body style="background-color: #e5e7eb">
+<body style="background-color: #e5e7eb">
     <header>
         <div class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
-            <a href="#" class="navbar-brand d-flex align-items-center">
+            <a href="{{ route('home.index') }}" class="navbar-brand d-flex align-items-center">
                 <img src="{{ asset('/img/portfolio/logo.png') }}" alt="logo" />
                 <strong>Gente PreValente</strong>
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+                aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -49,12 +39,12 @@
 
                     <div class="navbar-brand d-flex align-items-center">
                         <img src="{{ asset('/img/portfolio/lupa.png') }}" alt="lupa" />
-                        <input type="text" name="search" Placeholder="Buscar..." class ="search-txt">
+                        <input type="text" name="search" Placeholder="Buscar..." class="search-txt">
                     </div>
 
                     <div class="navbar-brand d-flex align-items-center">
                         <img src="{{ asset('/img/portfolio/admin.png') }}" />
-                        <p class="text-header">Administración</p><span class="num">2</span>
+                        <p class="text-header">Administración</p><span class="badge badge-pill badge-danger">s2</span>
                     </div>
 
                     <div class="navbar-brand d-flex align-items-center">
@@ -87,73 +77,17 @@
             </div>
         </div>
     </header>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <div class="card mb-5 shadow bg-white">
-                        <div class="box"><img src="{{ asset('/img/portfolio/company.png') }}" class="card-img"/></div>
-                        <div class="card-block">
-                            <h4 class="card-title">Mirion Jones</h4>
-                        </div>
-                        <div class="card-footer text-muted">
-                            <img src="{{ asset('/img/portfolio/time.png') }}" class="footer-card-img"/>
-                            2 days ago
-                          </div>
-                    </div>
-                </div>
 
-                <div class="col-12 col-md-6">
-                    <div class="card mb-5 shadow bg-white">
-                        <div class="box"><img src="{{ asset('/img/portfolio/graph.png') }}" class="card-img"/></div>
-                        <div class="card-block">
-                            <h4 class="card-title">Mirion Jones</h4>
-                        </div>
-                        <div class="card-footer text-muted">
-                            <img src="{{ asset('/img/portfolio/time.png') }}" class="footer-card-img"/>
-                            2 days ago
-                          </div>
-                    </div>
-                </div>
-
-                <div class="col-12 col-md-6">
-                    <div class="card mb-5 shadow bg-white">
-                        <div class="box"><img src="{{ asset('/img/portfolio/staff.png') }}" class="card-img"/></div>
-                        <div class="card-block">
-                            <h4 class="card-title">Mirion Jones</h4>
-                        </div>
-                        <div class="card-footer text-muted">
-                            <img src="{{ asset('/img/portfolio/time.png') }}" class="footer-card-img"/>
-                            2 days ago
-                          </div>
-                    </div>
-                </div>
-
-                <div class="col-12 col-md-6">
-                    <div class="card mb-5 shadow bg-white">
-                        <div class="box"><img src="{{ asset('/img/portfolio/users.png') }}" class="card-img"/></div>
-                        <div class="card-block">
-                            <h4 class="card-title">Mirion Jones</h4>
-                        </div>
-                        <div class="card-footer text-muted">
-                            <img src="{{ asset('/img/portfolio/time.png') }}" class="footer-card-img"/>
-                            2 days ago
-                          </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    @yield('content')
 
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+    <script src="/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-{{-- <footer class="text-muted">
-  <div class="container">
-    <p class="float-right">
-      <a href="#">Back to top</a>
-    </p>
-    <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    <p>New to Bootstrap? <a href="https://getbootstrap.com/">Visit the homepage</a> or read our <a href="/docs/4.5/getting-started/introduction/">getting started guide</a>.</p>
-  </div>
-</footer> --}}
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script></body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
